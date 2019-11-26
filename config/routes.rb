ffb8e3f57dd55902226c9a976181854be76b1ade
Bill Leezer <bill.leezer@gmail.com>
2019-11-26 10:24:21 -0500

    Initial Commit Stocking2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# in app/config/routes.rb
+
+Rails.application.routes.draw do
+  # get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see 
+  # sign represents route/routes here for window url's.
+end
