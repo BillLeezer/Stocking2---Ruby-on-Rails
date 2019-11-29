d6e9d49b0ce4b8c766a58569a7ffe4128f6580d4
BillLeezer <57925444+BillLeezer@users.noreply.github.com>
2019-11-28 21:11:56 -0500

    All Devise forms formated with a functional Conditional Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 # in app/config/routes.rb
-
 Rails.application.routes.draw do
   devise_for :users
   # get 'home/index'
   root 'home#index'
-
   get 'home/about'
-
   post "/" => 'home#index'
-
   # For details on the DSL available within this file, see 
   # sign represents route/routes here for window url's.
 end
